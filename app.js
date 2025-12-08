9a8eecd3e93c1ccd8424398bf72f69347e7f0476
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2025-12-08 23:53:08 +0000

    seventh commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(cors({
-    origin:true,
+    origin:'https://freshy-food-frontend.vercel.app',
     credentials: true
 }))
 
